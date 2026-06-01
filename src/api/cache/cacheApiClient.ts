26a45c1ffff42feac059b2c23a9623f4deb37394
joshuarreid <2001josh@gmail.com>
2026-06-01 19:59:23 -0400

    fix

--- src/api/cache/cacheApiClient.ts
+++ src/api/cache/cacheApiClient.ts
@@ -1,11 +1,7 @@
 import { ApiClient } from '../ApiClient'
 
-/**
- * Cache endpoints are not under /api/v2 and are not JWT-protected.
- */
-export const cacheApiClient = new ApiClient(
-  'https://wmb-service-fs4j9.ondigitalocean.app'
-)
+
+export const cacheApiClient = new ApiClient()
 
 export const cacheBasePath = '/api/cache'
 
